d7ce36bb290d5ff9d27d90970bf9d07dfc186267
aXenDeveloper <ithereplay@gmail.com>
2021-09-02 18:43:28 +0200

    Change to useQuery in recpies view

--- pages/recipes/index.tsx
+++ pages/recipes/index.tsx
@@ -1,38 +1,17 @@
-import { FC } from 'react';
 import { NextSeo } from 'next-seo';
 import useTranslation from 'next-translate/useTranslation';
 import { Layout } from '../../components/layouts/Layout';
-import { RecipeModel } from '../../types/database/RecipesType';
 import { RecipesView } from '../../views/recipes/RecipesView';
 
-interface Props {
-  recipes: RecipeModel[];
-}
-
-const RecipesPage: FC<Props> = ({ recipes }) => {
+const RecipesPage = () => {
   const { t } = useTranslation('global');
 
   return (
     <Layout>
       <NextSeo title={t('title_seo_page', { title: t('navigation_recipes') })} />
-      <RecipesView recipes={recipes} />
+      <RecipesView />
     </Layout>
   );
 };
-
-export async function getServerSideProps() {
-  const res = await fetch(
-    `${
-      process.env.NODE_ENV !== 'production' ? 'http://localhost:3000' : process.env.NEXTAUTH_URL
-    }/api/recipes`
-  );
-  const data = await res.json();
-
-  return {
-    props: {
-      recipes: data.results
-    }
-  };
-}
 
 export default RecipesPage;
--- views/recipes/RecipesView.tsx
+++ views/recipes/RecipesView.tsx
@@ -1,16 +1,34 @@
 import useTranslation from 'next-translate/useTranslation';
-import { FC } from 'react';
+import { useQuery } from 'react-query';
 import { Breadcrumb } from '../../components/Breadcrumb';
 import { Container } from '../../components/layouts/Container';
+import { SpinnersLoading } from '../../components/loading/SpinnersLoading';
+import { MessageBox } from '../../components/MessageBox';
 import { RecipesListItem } from '../../components/recipes/list/RecipesListItem';
 import { RecipeModel } from '../../types/database/RecipesType';
 
-interface Props {
-  recipes: RecipeModel[];
-}
-
-export const RecipesView: FC<Props> = ({ recipes }) => {
+export const RecipesView = () => {
   const { t } = useTranslation('global');
+
+  const { isLoading, isError, data } = useQuery<RecipeModel[]>('recipeList', async () => {
+    const res = await fetch(
+      `${
+        process.env.NODE_ENV !== 'production' ? 'http://localhost:3000' : process.env.NEXTAUTH_URL
+      }/api/recipes`
+    );
+    const resData = await res.json();
+
+    return resData.results;
+  });
+
+  if (isError) {
+    return (
+      <>
+        <Breadcrumb>{t('navigation_recipes')}</Breadcrumb>
+        <MessageBox code="5C103/1" />
+      </>
+    );
+  }
 
   return (
     <>
@@ -21,11 +39,15 @@ export const RecipesView: FC<Props> = ({ recipes }) => {
             <h1>{t('navigation_recipes')}</h1>
           </div>
 
-          <ul className="box padding recipes_list">
-            {recipes.map(el => (
-              <RecipesListItem key={el.id} recipe={el} />
-            ))}
-          </ul>
+          {isLoading ? (
+            <div className="box padding text_center">
+              <SpinnersLoading />
+            </div>
+          ) : (
+            <ul className="box padding recipes_list">
+              {data && data.map(el => <RecipesListItem key={el.id} recipe={el} />)}
+            </ul>
+          )}
         </main>
 
         <aside className="container_column:aside">
