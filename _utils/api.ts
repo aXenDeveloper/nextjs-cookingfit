4d26d6c46bb9e92f46bfb61a12120d01daaf96f6
aXenDeveloper <axendeveloper@gmail.com>
2023-03-03 18:33:59 +0100

    Change API url

--- _utils/api.ts
+++ _utils/api.ts
@@ -1,3 +1,1 @@
-export const apiURL = `${
-  process.env.NODE_ENV !== 'production' ? 'http://localhost:3000' : process.env.NEXTAUTH_URL
-}/api`;
+export const apiURL = `${process.env.NODE_ENV !== 'production' ? 'http://localhost:3000' : ''}/api`;
